381ac28923e9bc3ef30e490ab5489e280890562b
leandrosfreitas <leandrosanfreitas@gmail.com>
2025-02-27 23:00:26 -0300

    modificando alguns dados

--- day2.js
+++ day2.js
@@ -1,15 +1,15 @@
-const prompt = require('prompt-sync')();
+const nome = prompt('Qual o seu nome? ');
+const idade = prompt('Quantos anos você tem? ');
+const linguagem = prompt('Qual linguagem de programação você está estudando? ');
 
-let nome = prompt('Qual o seu nome? ');
-let idade = prompt('Quantos anos você tem? ');
-let linguagem = prompt('Qual linguagem de programação você está estudando? ');
+alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}`)
 
-console.log(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}`)
+const pergunta = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO `)
 
-let pergunta = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO `)
+if (pergunta === '1') {
+    alert('Muito bom! Continue estudando e você terá muito sucesso.')
+}
 
-if (pergunta == 1) {
-    console.log('Muito bom! Continue estudando e você terá muito sucesso.')
-} else {
-    console.log('Ahh que pena... Já tentou aprender outras linguagens?')
+if (pergunta === '2') {
+    alert('Ahh que pena... Já tentou aprender outras linguagens?')
 }
